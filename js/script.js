cdad7dc617eece378f6664ef9e303a54a2b66030
npsbranches <45974239+npsbranches@users.noreply.github.com>
2019-02-18 22:32:21 +0500

    3.0 Final

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$('.btn-bars').click(function(){
+    $('.menu').toggleClass('active');
+    $('.btn-bars').toggleClass('active');
+})
+
+$('.menu li a').click(function() {
+    $('.menu').toggleClass('active');
+    $('.btn-bars').toggleClass('active');
+})
